96dce4a19f55f379f1cd0cca45ad77957293313f
Trev <greenwoodtrevon@gmail.com>
2021-06-06 13:11:20 -0600

    boxes keep their classes when clicked for input

--- Develop/script.js
+++ Develop/script.js
@@ -5,8 +5,6 @@ var descriptionEl = $(".description");
 
 var sos = false;
 
-
-
 var nineA = $("#9A");
 var tenA = $("#10A");
 var elevA = $("#11A");
@@ -28,7 +26,6 @@ var displayDate = function() {
 };
 
 var checkTime = function(hour, section) {
-  console.log(currentHour24);
   if (hour < currentHour24) {
     section.addClass("past");
   };
@@ -40,20 +37,51 @@ var checkTime = function(hour, section) {
   };
 };
 
+var checkAllTimes = function() {
+  checkTime('09', nineA);
+  checkTime('10', tenA);
+  checkTime('11', elevA);
+  checkTime('12', twelP);
+  checkTime('13', oneP);
+  checkTime('14', twoP);
+  checkTime('15', thrP);
+  checkTime('16', fourP);
+  checkTime('17', fiveP);
+  checkTime('18', sixP);
+  checkTime('19', sevP);
+  checkTime('20', eightP);
+  checkTime('21', nineP);
+  checkTime('22', tenP);
+  checkTime('23', elevP);
+};
+
+$('.description').on('click', function(){
+  var className = $(this).attr('class')
+  console.log(className);
+  var $el = $(this);
+              
+  var task = $('<input/>')
+    .addClass(className)
+    .val($el.text());
+
+  $el.replaceWith(task);
+  
+  var save = function(){
+    var $p = $('<p/>')
+      .addClass(className)
+      .text(task.val());
+    task.replaceWith($p);
+    
+  };
+  task.on('blur', save).focus();
+  checkAllTimes();
+});
+
+$(".saveBtn").on("click", function() {
+  console.log('okie dokie');
+
+});
 
 displayDate();
-checkTime('09', nineA);
-checkTime('10', tenA);
-checkTime('11', elevA);
-checkTime('12', twelP);
-checkTime('13', oneP);
-checkTime('14', twoP);
-checkTime('15', thrP);
-checkTime('16', fourP);
-checkTime('17', fiveP);
-checkTime('18', sixP);
-checkTime('19', sevP);
-checkTime('20', eightP);
-checkTime('21', nineP);
-checkTime('22', tenP);
-checkTime('23', elevP);
+checkAllTimes();
+
